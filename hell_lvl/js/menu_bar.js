a0527dd2e047edf5073a62c8eaa45b175c60cfa5
Oleg <olegtupicyn62@gmail.com>
2019-06-20 03:30:22 +0300

    Menu_bar JS

--- hell_lvl/js/menu_bar.js
+++ hell_lvl/js/menu_bar.js
@@ -1,6 +1,7 @@
 var menuElem = document.getElementById('menu_bar');
 var titleElem = menuElem.querySelector('.navigator__menu--titel');
 
+
 titleElem.onclick = function() {
     menuElem.classList.toggle('open');
 };
